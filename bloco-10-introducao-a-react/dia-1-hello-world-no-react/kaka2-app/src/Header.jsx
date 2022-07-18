5c14f8b496c063b739e4b7f0b475e42ea90e4660
kakaleal <lealk530@gmail.com>
2022-07-18 20:28:45 -0300

    Subindo exercicios

---
+++ bloco-10-introducao-a-react/dia-1-hello-world-no-react/kaka2-app/src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Header extends React.Component{
+  render(){
+    return(<span>
+      <h1>Conteudos de Front-End</h1>
+    </span>)
+  }
+}
+
+export default Header;
